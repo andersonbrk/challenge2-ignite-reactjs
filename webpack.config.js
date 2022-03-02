7db9538a5259b1b5e9514d0bbe5d9733f8e35806
andersonbrk <anderson.santos@saibweb.com.br>
2022-03-02 16:54:32 -0300

    Challenge finished!!!

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
     hot: true,
   },
   plugins: [
-    isDevelopment && new ReactRefreshWebpackPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin({ overlay: false }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
